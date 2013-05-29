015df75d2bda1325955cae4ba935dd50fe26915d
aydin <aydin@ubuntu>
2013-05-29 08:04:40 -0700

    first stab at an adapter for dropbox-php-sdk
    Needs some work on the exception catching and handling, not sure how to do it properly.

---
+++ src/Gaufrette/Adapter/DropboxSdk.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Gaufrette\Adapter;
+
+use Gaufrette\Adapter;
+use Gaufrette\Util;
+use Gaufrette\Exception;
+use Dropbox\Client;
+use Dropbox\WriteMode;
+
+
+//use Dropbox_API as DropboxApi;
+//use \Dropbox_Exception_NotFound as DropboxNotFoundException;
+
+/**
+ * Dropbox adapter
+ *
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ * @author Antoine Hérault <antoine.herault@gmail.com>
+ * @author Leszek Prabucki <leszek.prabucki@gmail.com>
+ */
+class Dropbox implements Adapter
+{
+    protected $client;
+
+    /**
+     * Constructor
+     *
+     * @param \Dropbox_API $client The Dropbox API client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \Dropbox_Exception_Forbidden
+     * @throws \Dropbox_Exception_OverQuota
+     * @throws \OAuthException
+     */
+    public function read($key)
+    {
+        try {
+            $resource = tmpfile();
+            $this->client->getFile($key, $resource, null);
+            $data = fread($resource, 1024);
+            fclose($resource);
+            return $data;
+        } catch (DropboxNotFoundException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isDirectory($key)
+    {
+        try {
+            $metadata = $this->getDropboxMetadata($key);
+        } catch (Exception\FileNotFound $e) {
+            return false;
+        }
+
+        return (boolean) isset($metadata['is_dir']) ? $metadata['is_dir'] : false;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \Dropbox_Exception
+     */
+    public function write($key, $content)
+    {
+       
+        try {
+            $this->client->uploadFileFromString($key, WriteMode::Add(), $content);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return Util\Size::fromContent($content);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($key)
+    {
+        try {
+            $this->client->delete($key);
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rename($sourceKey, $targetKey)
+    {
+        try {
+            $this->client->move($sourceKey, $targetKey);
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function mtime($key)
+    {
+        try {
+            $metadata = $this->getDropboxMetadata($key);
+        } catch (Exception\FileNotFound $e) {
+            return false;
+        }
+
+        return strtotime($metadata['modified']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function keys()
+    {
+        $metadata = $this->client->getMetadataWithChildren('/');
+        if (! isset($metadata['contents'])) {
+            return array();
+        }
+
+        $keys = array();
+        foreach ($metadata['contents'] as $value) {
+            $file = ltrim($value['path'], '/');
+            $keys[] = $file;
+            if ('.' !== dirname($file)) {
+                $keys[] = dirname($file);
+            }
+        }
+        sort($keys);
+
+        return $keys;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exists($key)
+    {
+        try {
+            $this->getDropboxMetadata($key);
+
+            return true;
+        } catch (Exception\FileNotFound $e) {
+            return false;
+        }
+    }
+
+    private function getDropboxMetadata($key)
+    {
+        try {
+            $metadata = $this->client->getMetadata($key);
+        } catch (\Exception $e) {
+            throw new Exception\FileNotFound($key, 0, $e);
+        }
+
+        // TODO find a way to exclude deleted files
+        if (isset($metadata['is_deleted']) && $metadata['is_deleted']) {
+            throw new Exception\FileNotFound($key);
+        }
+
+        return $metadata;
+    }
+}
